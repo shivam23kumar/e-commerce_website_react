715dc327ac9aa88efcc7f7826f8808041c2570db
Shivam Kumar <shivamkumar.dev23@gmail.com>
2020-07-22 11:07:54 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import HomePage from './components/pages/homepage.component';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+      
+    </div>
+  );
+}
+
+export default App;
